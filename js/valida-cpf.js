95b41b6c700933fa00df6edb0fe3a769de4c988d
Debiason <debiasonw@gmail.com>
2024-03-10 16:08:21 -0300

    validando cpf e data

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,12 +1,12 @@
-export default function ehUmCpf(campo) {
+export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumeroRepetidos(cpf);
-    validaPrimeiroDigito(cpf);
-
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse CPF não é valido!');
+    }
 }
 
-function validaNumeroRepetidos(cpf){
-    const numeoRepetidos = [
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
         '00000000000',
         '11111111111',
         '22222222222',
@@ -16,27 +16,44 @@ function validaNumeroRepetidos(cpf){
         '66666666666',
         '77777777777',
         '88888888888',
-        '99999999999',
-        '00000000000'
+        '99999999999'
     ]
 
-    return numeoRepetidos.includes(cpf);
+    return numerosRepetidos.includes(cpf)
 }
 
-function validaPrimeiroDigito(cpf){
-    let soma = 0 ;
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
     let multiplicador = 10;
 
     for (let tamanho = 0; tamanho < 9; tamanho++) {
         soma += cpf[tamanho] * multiplicador;
-        multiplicador--;
+        multiplicador--
     }
 
     soma = (soma * 10) % 11;
 
-    if (soma == 10 || soma == 11 ) {
+    if (soma == 10 || soma == 11) {
         soma = 0;
     }
 
-    return soma != cpf[9]
+    return soma != cpf[9];
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0;
+    }
+
+    return soma != cpf[10];
 }
---
+++ js/valida-idade.js
@@ -0,0 +1,14 @@
+export default function ehMaiorDeIdade(campo){
+    const dataNascimento = new Date(campo.value);
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
+}
+
+function validaIdade(data){
+    const dataAtual = new Date();
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
+
+    console.log(dataAtual >= dataMais18);
+    return dataAtual >= dataMais18;
+}
